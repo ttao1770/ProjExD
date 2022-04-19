a4440cb784968ac11d9d684fa6efc9c79c45951b
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-04-19 16:38:30 +0900

    listを使ったやつ

--- rensyu01/quiz.py
+++ rensyu01/quiz.py
@@ -1,24 +1,22 @@
-from random import randint 
+from random import choices, randint 
 import datetime
+from secrets import choice
 if __name__ == "__main__":
 
-    #問題と解答を羅列する
-    question = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
-    answer = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
+    #問題をキー，正解リストを値とした辞書
+    qa_dict = {"サザエの旦那の名前は？" : ["マスオ","ますお"],"カツオの妹の名前は？":["ワカメ","わかめ"],"タラオはカツオから見てどんな関係？":["甥","おい","甥っ子","おいっこ"]}
 
-    num = randint(0,2) #listの番号
+    ans = choice(list(qa_dict.items())) #listの番号
 
-    print("問題",question[num])
+    print("問題",ans[0])
 
     st = datetime.datetime.now()
-
-    
 
     while True :
 
         item = input("答えを入力してください")
 
-        if item in answer[num] :
+        if item in ans[1] :
             ed = datetime.datetime.now()
             print("正解")
             break
@@ -27,3 +25,4 @@ if __name__ == "__main__":
             print("不正解。もう一度")
 
     print(f"所要時間は{(ed-st).seconds}秒です。")
+    
