133be24aa6d8f995977dbb1c2ad4dfd039955288
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-04-19 15:57:45 +0900

    listを使ったやつ

---
+++ rensyu01/quiz.py
@@ -0,0 +1,29 @@
+from random import randint 
+import datetime
+if __name__ == "__main__":
+
+    #問題と解答を羅列する
+    question = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+    answer = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
+
+    num = randint(0,2) #listの番号
+
+    print("問題",question[num])
+
+    st = datetime.datetime.now()
+
+    
+
+    while True :
+
+        item = input("答えを入力してください")
+
+        if item in answer[num] :
+            ed = datetime.datetime.now()
+            print("正解")
+            break
+            
+        else :
+            print("不正解。もう一度")
+
+    print(f"所要時間は{(ed-st).seconds}秒です。")
