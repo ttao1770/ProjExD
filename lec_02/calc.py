6c7494ce84ba019f21329b6bee1400d77824909e
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-04-26 17:54:00 +0900

    プログラムの説明を追加 #5

--- lec_02/calc.py
+++ lec_02/calc.py
@@ -1,33 +1,42 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-from tkinter.tix import COLUMN
+from random import randint
+
+#イコールを押した際に計算結果を出力する
+def button_equrle(event):
+        num = entry.get()
+        res = eval(num)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END, str(res))
+
+#クリックしたときクリックした文字を画面に出力する
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    entry.insert(tk.END, txt)
+    #tkm.showinfo(txt,f"{txt}のボタンが押されました")
+
+#ｃボタンを押した際に画面の数字をすべて消す
+def button_color(event):
+    entry.delete(0,tk.END)
+
+#rdボタンを押した際にランダムに４桁までの数値を画面に出す
+def button_random(event):
+    num = randint(0,9999)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END, str(num))
 
 if __name__ == "__main__":
     
     root = tk.Tk()
     root.geometry("300x450")
 
-    #入力欄の設定
+#入力欄の設定
     entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
     
-    entry.grid(row=0,column=0,columnspan=3)
+    entry.grid(row=0,column=0,columnspan=4)
 
 #ボタンの設定
-    def button_equrle(event):
-        num = entry.get()
-        res = eval(num)
-        entry.delete(0,tk.END)
-        entry.insert(tk.END, str(res))
-
-    def button_click(event):
-        btn = event.widget
-        txt = btn["text"]
-        entry.insert(tk.END, txt)
-        #tkm.showinfo(txt,f"{txt}のボタンが押されました")
-
-    def button_color(event):
-        entry.delete(0,tk.END)
-
     c = 1
 
     for i in range(3):
@@ -36,18 +45,27 @@ if __name__ == "__main__":
             btn.bind("<1>",button_click)
             if j >= 2:
                 c += 2
-            btn.grid(row = i + 1, column = j, padx = 10, pady = 10)
+            btn.grid(row = i + 1, column = j)
+    #0のボタンを追加
     btn = tk.Button(root, text=0, font=("Times New Roman", 30))
-    btn.grid(row = 4, column = 0, padx = 10, pady = 10)
+    btn.grid(row = 4, column = 0)
     btn.bind("<1>",button_click)
+    #＋のボタンを追加
     btn = tk.Button(root, text = "+", font = ("Time New Roman",30))
-    btn.grid(row = 4, column = 1, padx = 10, pady = 10)
+    btn.grid(row = 4, column = 1)
     btn.bind("<1>",button_click)
+    #＝のボタンを追加
     btn = tk.Button(root, text = "=", font = ("Time New Roman", 30))
-    btn.grid(row = 4, column = 2, padx = 10, pady = 10)
+    btn.grid(row = 4, column = 2)
     btn.bind("<1>",button_equrle)
+    #cのボタンを追加
     btn = tk.Button(root, text= "c", font = ("Time New Roman", 30))
-    btn.grid(row = 1, column=3, padx=10, pady=10)
+    btn.grid(row = 1, column=3)
     btn.bind("<1>",button_color)
+    #ｒｄのボタンを追加
+    btn = tk.Button(root, text = "rd", font = ("Time New Roman", 30))
+    btn.grid(row = 2, column=3)
+    btn.bind("<1>",button_random)
+
     
     root.mainloop()
