072c5589d28a2d975f5f86fef9237685b0f253d1
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-04-26 15:43:24 +0900

    Entry

--- lec_02/calc.py
+++ lec_02/calc.py
@@ -7,15 +7,29 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x450")
 
+    #入力欄の設定
+    entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=3)
+
+#ボタンの設定
+    def button_click(event):
+        btn = event.widget
+        txt = btn["text"]
+        tkm.showinfo(txt,f"{txt}のボタンが押されました")
 
     c = 1
+
     for i in range(3):
          for j in range(3):
-            btn = tk.Button(root, text=i+j+c, font=("Times New Roman", 30))
+            btn = tk.Button(root, text=i+j+c, font=("Times New Roman", 30), command = button_click)
+            btn.bind("<1>",button_click)
             if j >= 2:
                 c += 2
-            btn.grid(row = i, column = j)
-    btn = tk.Button(root, text=0, font=("Times New Roman", 30))
-    btn.grid(row = 4, column = 0)
+            btn.grid(row = i + 1, column = j, padx = 10, pady = 10)
+    btn = tk.Button(root, text=0, font=("Times New Roman", 30),command = button_click)
+    btn.bind("<1>",button_click)
+    btn.grid(row = 4, column = 0, padx = 10, pady = 10)
+
+
     
     root.mainloop()
