bc15f52df3811585635e5dee187cdb4de33770b7
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-04-26 16:56:06 +0900

    計算ができるようになった

--- lec_02/calc.py
+++ lec_02/calc.py
@@ -13,13 +13,20 @@ if __name__ == "__main__":
     entry.grid(row=0,column=0,columnspan=3)
 
 #ボタンの設定
+    def keisan(event):
+        num = entry.get()
+        res = eval(num)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END, str(res))
+
     def button_click(event):
         btn = event.widget
         txt = btn["text"]
         entry.insert(tk.END, txt)
         #tkm.showinfo(txt,f"{txt}のボタンが押されました")
 
-
+    
+    
     c = 1
 
     for i in range(3):
@@ -31,11 +38,13 @@ if __name__ == "__main__":
             btn.grid(row = i + 1, column = j, padx = 10, pady = 10)
     btn = tk.Button(root, text=0, font=("Times New Roman", 30))
     btn.grid(row = 4, column = 0, padx = 10, pady = 10)
+    btn.bind("<1>",button_click)
     btn = tk.Button(root, text = "+", font = ("Time New Roman",30))
     btn.grid(row = 4, column = 1, padx = 10, pady = 10)
+    btn.bind("<1>",button_click)
     btn = tk.Button(root, text = "=", font = ("Time New Roman", 30))
     btn.grid(row = 4, column = 2, padx = 10, pady = 10)
-    btn.bind("<1>",button_click)
-    
+    btn.bind("<1>",keisan)
+
     
     root.mainloop()
