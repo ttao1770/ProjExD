be5c5a58b08c833c88196c3536b790b8b4e25d62
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-04-26 16:13:45 +0900

    イコールを入力できるようにした

--- lec_02/calc.py
+++ lec_02/calc.py
@@ -19,19 +19,23 @@ if __name__ == "__main__":
         entry.insert(tk.END, txt)
         #tkm.showinfo(txt,f"{txt}のボタンが押されました")
 
+
     c = 1
 
     for i in range(3):
          for j in range(3):
-            btn = tk.Button(root, text=i+j+c, font=("Times New Roman", 30), command = button_click)
+            btn = tk.Button(root, text=i+j+c, font=("Times New Roman", 30))
             btn.bind("<1>",button_click)
             if j >= 2:
                 c += 2
             btn.grid(row = i + 1, column = j, padx = 10, pady = 10)
-    btn = tk.Button(root, text=0, font=("Times New Roman", 30),command = button_click)
-    btn.bind("<1>",button_click)
+    btn = tk.Button(root, text=0, font=("Times New Roman", 30))
     btn.grid(row = 4, column = 0, padx = 10, pady = 10)
-
-
+    btn = tk.Button(root, text = "+", font = ("Time New Roman",30))
+    btn.grid(row = 4, column = 1, padx = 10, pady = 10)
+    btn = tk.Button(root, text = "=", font = ("Time New Roman", 30))
+    btn.grid(row = 4, column = 2, padx = 10, pady = 10)
+    btn.bind("<1>",button_click)
+    
     
     root.mainloop()
