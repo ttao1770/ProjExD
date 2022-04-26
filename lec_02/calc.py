de7dcb2f1bfbf122254d04e58ed536ca085784f4
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-04-26 15:09:44 +0900

    ボタンを追加

--- lec_02/calc.py
+++ lec_02/calc.py
@@ -1,9 +1,21 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+from tkinter.tix import COLUMN
 
 if __name__ == "__main__":
     
     root = tk.Tk()
     root.geometry("300x450")
+
+
+    c = 1
+    for i in range(3):
+         for j in range(3):
+            btn = tk.Button(root, text=i+j+c, font=("Times New Roman", 30))
+            if j >= 2:
+                c += 2
+            btn.grid(row = i, column = j)
+    btn = tk.Button(root, text=0, font=("Times New Roman", 30))
+    btn.grid(row = 4, column = 0)
     
-    root.mainloop
+    root.mainloop()
