0b6337185ce2a17c1040407e62a3d4ea42f96303
C0B21100 <c0b211003a@edu.teu.ac.jp>
2022-05-10 17:45:52 +0900

    ゴール作った

--- lec_03/rensyu03/maze.py
+++ lec_03/rensyu03/maze.py
@@ -14,19 +14,35 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx,cy,mx,my,key
+    global cx,cy,mx,my,key,c
     if key == "Up" and maze_bg[my-1][mx] == 0:
-        my -= 1
+        my -= c
     if key == "Down" and maze_bg[my+1][mx] == 0:
-        my += 1
+        my += c
     if key == "Right" and maze_bg[my][mx+1] == 0:
-        mx += 1
+        mx += c
     if key == "Left" and maze_bg[my][mx-1] == 0:
-        mx -= 1
+        mx -= c
     cx,cy = 100 * mx + 50, 100 * my +50
 
     root.after(100,main_proc)
     canvas.coords("tori", cx, cy)
+
+def make_startgoal():
+    canvas.create_rectangle(100,100,200,200,fill="green")
+    canvas.create_rectangle(1300,700,1400,800,fill="red")
+
+def even_fin():
+    global cx,cy,mx,my,key,c
+    if cx >= 1200 and cy >= 600:
+        goal = tk.PhotoImage(file = "fig/20.png")
+        cx,cy = 750,450
+        canvas.create_image(cx, cy, image=goal, tag = "goal")
+        c = 0
+    
+
+
+    
     
 
 
@@ -36,6 +52,7 @@ if __name__ == "__main__":
     key = ""
     my = 1
     mx = 1
+    c = 1
 
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -44,8 +61,12 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
 
+    
+
     maze_bg = mm.make_maze(15,9)
     mm.show_maze(canvas,maze_bg)
+
+    make_startgoal()
 
     
     tori = tk.PhotoImage(file = "fig/5.png")
@@ -54,6 +75,8 @@ if __name__ == "__main__":
     
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+
+    even_fin()
 
     
 
